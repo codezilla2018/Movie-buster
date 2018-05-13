f5666330d2526ec7b876e156d7d0fdf13cf36274
[isharamadusanka] <[sandaruwanishara96@gmail.com]>
2018-05-13 23:24:39 +0530

    third commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    consumer_key:         '4LItGk1lvmQRTW0NBnWdNpJ9V',
+    consumer_secret:      'ue5Tsxj173eDzwDrGge40vM6eLxyjLIXUjZKICLHOL28c0Sjsh',
+    access_token:         '3250257499-uZbqsGgxENfVbQPOwEScYSicQuEsRQcG5UQ4u5m',
+    access_token_secret:  'FktZIEZnY2fbETETj0Fg1Yt9QihW2mf7TqrNb1eLEiM4B',
+}
